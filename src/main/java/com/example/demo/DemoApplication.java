193ea2d1eded3d8287d9b106a6bb97b63dde8966
Qapitol QA <shashiwala.b@qapitol.com>
2019-06-22 20:19:25 +0530

    added message

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.logging.*;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -14,6 +15,8 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("hello shashi");
+		Logger.getLogger(Logger.GLOBAL_LOGGER_NAME).log(Level.INFO,"Welcome to Student Management application");
+
 	}
 
 }
